8e5c108aad9d137913dfa9e48679f0b89838e29e
maris <maris@LAPTOP-S2PGAQHR.speedport.ip>
2024-06-24 18:53:25 +0200

    Aufgabe 2 gelöst

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -153,6 +153,19 @@ public class Hanoi {
 
     private void movePieces(int numberOfPieces, char from, char to, char util) {
 	// TODO: Implement me!
+    	//Basisfall:
+    	if(numberOfPieces == 1) {
+    		movePiece(from, to);
+    	}
+    	
+    	//Rekursionsschritt:
+    	
+    	else{
+    		movePieces(numberOfPieces - 1, from, util, to);
+    		movePiece(from, to);
+    		movePieces(numberOfPieces - 1, util, to, from);	
+    	}
+
     }
 
 }
