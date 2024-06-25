d064d4cfcdc27328dc6fc3ba71b85668d9be4bab
DaveSanti <dgolomb94@gmail.com>
2024-06-25 15:00:17 +0200

    Excercise 08

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -12,147 +12,121 @@ public class Hanoi {
     Deque<Integer> middle = new LinkedList<Integer>();
     Deque<Integer> right = new LinkedList<Integer>();
 
-    /**
-     * Constructor to create a new board. The left stick is filled with the correct
-     * set of pieces.
-     */
     public Hanoi() {
-	for (int i = 3; i < 10; i++) {
-	    left.addLast(i);
-	}
+        for (int i = 3; i < 10; i++) {
+            left.addLast(i);
+        }
     }
 
-    /**
-     * Private method to move one piece from one stick to another. The two arguments
-     * from and two are `char` values l, m, or r for the left, middle, or right
-     * stick. Any other supplied values will throw an IllegalArgumentException.
-     * 
-     * @param from
-     * @param to
-     */
     private void movePiece(char from, char to) {
+        Integer piece = null;
+        switch (from) {
+            case 'l':
+                piece = left.remove();
+                break;
+            case 'm':
+                piece = middle.remove();
+                break;
+            case 'r':
+                piece = right.remove();
+                break;
+            default:
+                throw new IllegalArgumentException("Argument " + from + " is not allowed.");
+        }
 
-	Integer piece = null;
-	switch (from) {
-	case 'l':
-	    piece = left.remove();
-	    break;
-	case 'm':
-	    piece = middle.remove();
-	    break;
-	case 'r':
-	    piece = right.remove();
-	    break;
-	default:
-	    throw new IllegalArgumentException("Argument " + from + " is not allowed.");
-	}
-
-	switch (to) {
-	case 'l':
-	    left.addFirst(piece);
-	    break;
-	case 'm':
-	    middle.addFirst(piece);
-	    break;
-	case 'r':
-	    right.addFirst(piece);
-	    break;
-	default:
-	    throw new IllegalArgumentException("Argument " + to + " is not allowed.");
-	}
+        switch (to) {
+            case 'l':
+                left.addFirst(piece);
+                break;
+            case 'm':
+                middle.addFirst(piece);
+                break;
+            case 'r':
+                right.addFirst(piece);
+                break;
+            default:
+                throw new IllegalArgumentException("Argument " + to + " is not allowed.");
+        }
     }
 
-    /**
-     * Main function to play the game manually. Users enter two-symbol combinations
-     * that describe source and target stick.
-     */
     public void run() {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	while (true) {
-	    try {
-		System.out.println(this);
-		System.out.print("Enter source and target stick (will move top piece):");
-		String s = br.readLine();
-		if (s.matches("^([lmr])([lmr])$")) {
-		    char source = s.charAt(0);
-		    char target = s.charAt(1);
-		    movePiece(source, target);
-		}
-	    } catch (Exception e) {
-		System.out.println("Try again, something's not right.");
-		e.printStackTrace();
-	    }
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.println(this);
+                System.out.print("Enter source and target stick (will move top piece):");
+                String s = br.readLine();
+                if (s.matches("^([lmr])([lmr])$")) {
+                    char source = s.charAt(0);
+                    char target = s.charAt(1);
+                    movePiece(source, target);
+                }
+            } catch (Exception e) {
+                System.out.println("Try again, something's not right.");
+                e.printStackTrace();
+            }
+        }
     }
 
     private Iterator<Integer> getLeftDescendingIterator() {
-	return left.descendingIterator();
-
+        return left.descendingIterator();
     }
 
     private Iterator<Integer> getMiddleDescendingIterator() {
-	return middle.descendingIterator();
-
+        return middle.descendingIterator();
     }
 
     private Iterator<Integer> getRightDescendingIterator() {
-	return right.descendingIterator();
+        return right.descendingIterator();
     }
 
-    /**
-     * Method to print out the current state of the board in the terminal.
-     */
     @Override
     public String toString() {
-	StringBuilder b = new StringBuilder();
-	b.append("  |\n l|");
-	Iterator<Integer> iter;
-	iter = this.getLeftDescendingIterator();
-	while (iter.hasNext()) {
-	    b.append(iter.next());
-	    b.append(' ');
-	}
-	b.append("\n  |\n m|");
-	iter = this.getMiddleDescendingIterator();
-	while (iter.hasNext()) {
-	    b.append(iter.next());
-	    b.append(' ');
-	}
-	b.append("\n  |\n r|");
-	iter = this.getRightDescendingIterator();
-	while (iter.hasNext()) {
-	    b.append(iter.next());
-	    b.append(' ');
-	}
-	b.append("\n  |");
-	return b.toString();
+        StringBuilder b = new StringBuilder();
+        b.append("  |\n l|");
+        Iterator<Integer> iter;
+        iter = this.getLeftDescendingIterator();
+        while (iter.hasNext()) {
+            b.append(iter.next());
+            b.append(' ');
+        }
+        b.append("\n  |\n m|");
+        iter = this.getMiddleDescendingIterator();
+        while (iter.hasNext()) {
+            b.append(iter.next());
+            b.append(' ');
+        }
+        b.append("\n  |\n r|");
+        iter = this.getRightDescendingIterator();
+        while (iter.hasNext()) {
+            b.append(iter.next());
+            b.append(' ');
+        }
+        b.append("\n  |");
+        return b.toString();
     }
 
     public static void main(String[] args) {
-	Hanoi hanoi = new Hanoi();
-	// hanoi.run();
-	hanoi.runAutomatically();
+        Hanoi hanoi = new Hanoi();
+        // hanoi.run();
+        hanoi.runAutomatically();
     }
 
-    /**
-     * The entry point into the automatic solution. The method will print out the
-     * initial situation, call {@link #movePieces(int, char, char, char)}, and print
-     * the final situation. The recursive implementation of the solution goes into
-     * {@link #movePieces(int, char, char, char)}.
-     */
     private void runAutomatically() {
-	// we print out the initial situation
-	System.out.println(this);
-
-	// this is the entry point into the recursion
-	movePieces(left.size(), 'l', 'r', 'm');
-
-	// and in the end, we check that everything has been moved
-	System.out.println(this);
+        System.out.println(this);
+        movePieces(left.size(), 'l', 'r', 'm');
+        System.out.println(this);
     }
 
     private void movePieces(int numberOfPieces, char from, char to, char util) {
-	// TODO: Implement me!
+        if (numberOfPieces == 1) {
+            movePiece(from, to);
+            System.out.println(this); // Optional: Print after each move
+        } else {
+            movePieces(numberOfPieces - 1, from, util, to);
+            movePiece(from, to);
+            System.out.println(this); // Optional: Print after each move
+            movePieces(numberOfPieces - 1, util, to, from);
+        }
     }
-
 }
--- src/idh/java/Tree.java
+++ src/idh/java/Tree.java
@@ -6,90 +6,56 @@ import java.util.Set;
 
 public class Tree<T> {
 
-    /**
-     * The payload value for this tree node
-     */
     T value;
-
-    /**
-     * The set of children this tree node has
-     */
     Set<Tree<T>> children;
 
-    /**
-     * Constructs a new tree without children.
-     * 
-     * @param value The payload value
-     */
     public Tree(T value) {
-	this.value = value;
-	this.children = new HashSet<Tree<T>>();
+        this.value = value;
+        this.children = new HashSet<Tree<T>>();
     }
 
-    /**
-     * Constructs a new tree with children.
-     * 
-     * @param value    The payload value
-     * @param children The children. Note that children are represented as set
-     *                 internally, i.e., each child node is unique. Also note that
-     *                 the children are of type Tree<T>.
-     */
     public Tree(T value, Collection<Tree<T>> children) {
-	this.value = value;
-	this.children = new HashSet<Tree<T>>(children);
+        this.value = value;
+        this.children = new HashSet<Tree<T>>(children);
     }
 
-    /**
-     * Return the payload value of this tree node
-     * 
-     * @return The payload value
-     */
     public T get() {
-	return value;
+        return value;
     }
 
-    /**
-     * Set the payload value of this tree node
-     * 
-     * @param value The new value
-     */
     public void set(T value) {
-	this.value = value;
+        this.value = value;
     }
 
-    /**
-     * Return the set of children
-     * 
-     * @return
-     */
     public Set<Tree<T>> children() {
-	return children;
+        return children;
     }
 
-    /**
-     * Iterate over the tree, printing out each node value
-     */
-    public void dfs() {
-	System.out.println(this.value);
-	for (Tree<T> child : children) {
-	    child.dfs();
-	}
+    public void dfs(int depth) {
+        StringBuilder indent = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            indent.append("  ");
+        }
+        System.out.println(indent.toString() + this.value);
+        for (Tree<T> child : children) {
+            child.dfs(depth + 1);
+        }
     }
 
     public static void main(String[] args) {
 
-	Tree<String> ebike = new Tree<String>("e-bike");
-	Tree<String> tandem = new Tree<String>("tandem");
-	Tree<String> bike = new Tree<String>("bike");
-	Tree<String> buggy = new Tree<String>("buggy");
-	Tree<String> wheeled_vehicle = new Tree<String>("wheeled vehicle");
+        Tree<String> ebike = new Tree<String>("e-bike");
+        Tree<String> tandem = new Tree<String>("tandem");
+        Tree<String> bike = new Tree<String>("bike");
+        Tree<String> buggy = new Tree<String>("buggy");
+        Tree<String> wheeled_vehicle = new Tree<String>("wheeled vehicle");
 
-	wheeled_vehicle.children().add(bike);
-	wheeled_vehicle.children().add(buggy);
-	bike.children().add(tandem);
-	bike.children().add(ebike);
+        bike.children().add(tandem);
+        bike.children().add(ebike);
+        wheeled_vehicle.children().add(bike);
+        wheeled_vehicle.children().add(buggy);
 
-	wheeled_vehicle.dfs();
+        wheeled_vehicle.dfs(0);
     }
 
 }
