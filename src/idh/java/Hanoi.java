263e3e90906fd1aa01b0bfb5eef585d303ba7b43
linea <linea@PCLF.local>
2024-06-26 10:26:49 +0200

    Funktionen die sich selber aufrufen

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -152,7 +152,19 @@ public class Hanoi {
     }
 
     private void movePieces(int numberOfPieces, char from, char to, char util) {
-	// TODO: Implement me!
-    }
+    	if(left.isEmpty()&& middle.isEmpty()) {
+    		System.out.println(this);
+    	}
+    	else if (numberOfPieces == 1) {
+            movePiece(from, to);
+            System.out.println(this);
+        } else {
+            movePieces(numberOfPieces - 1, from, util, to);
+            movePiece(from, to);
+            System.out.println(this);
+            movePieces(numberOfPieces - 1, util, to, from);
+        }
+	 }
+    
 
 }
